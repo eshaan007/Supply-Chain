04cca3d81f27f86a446134f092e704833c1472b9
eshaan007 <eshaany2k@gmail.com>
2020-11-04 07:55:14 +0530

    :rocket:

---
+++ client/src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import ItemManager from "./contracts/ItemManager.json";
+import Item from "./contracts/Item.json";
+import getWeb3 from "./getWeb3";
+import "./App.css";
+
+class App extends Component {
+  state = { cost: 0, itemName: "exampleitem1", loaded: false };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      this.web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      this.accounts = await this.web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await this.web3.eth.net.getId();
+      
+      this.itemManager = new this.web3.eth.Contract(
+          ItemManager.abi,
+          ItemManager.networks[networkId] && ItemManager.networks[networkId].address,
+      );
+      this.item = new this.web3.eth.Contract(
+        Item.abi,
+        Item.networks[networkId] && Item.networks[networkId].address,
+      );
+      
+      this.setState({loaded:true});
+      
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  runExample = async () => {
+    const { accounts, contract } = this.state;
+
+    // Stores a given value, 5 by default.
+    await contract.methods.set(100).send({ from: accounts[1] });
+
+    // Get the value from the contract to prove it worked.
+    const {response } = await contract.methods.get().call();
+
+    // Update state with the result.
+    this.setState({ storageValue: response });
+  };
+
+  render() {
+    if (!this.state.loaded) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+        <h1>Good to Go!</h1>
+        <p>Your Truffle Box is installed and ready.</p>
+        <h2>Smart Contract Example</h2>
+        <p>
+          If your contracts compiled and migrated successfully, below will show
+          a stored value of 5 (by default).
+        </p>
+        <p>
+          Try changing the value stored on <strong>line 40</strong> of App.js.
+        </p>
+        <div>The stored value is: {this.state.storageValue}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
